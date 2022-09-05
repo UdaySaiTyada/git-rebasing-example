9d1452d60dec1ebd4ab12343a5a0faac81635516
Uday Sai Tyada <uday@Udays-MacBook-Pro-2.local>
2022-09-05 15:36:47 +0530

    Done

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello")
 print("Second hello")
 
 print("third")
+=======
+print("Hello")
+>>>>>>> parent of 6edf07d (Done)
 =======
 print("Hello")
 >>>>>>> parent of 6edf07d (Done)
